dc204dc7d4cbe62cfccd83c245d875e613beb18d
Ibrahim <61193755+Ibrahimx29@users.noreply.github.com>
2024-12-13 09:50:21 +0700

    kembalikan ke versi awal sebelum ada write

--- www/zebra-rfid.js
+++ www/zebra-rfid.js
@@ -1,19 +1,19 @@
-var exec = require("cordova/exec");
+var exec = require('cordova/exec');
 
 exports.init = function (arg0, success, error) {
-  exec(success, error, "zebraRfid", "init", [arg0]);
+    exec(success, error, 'zebraRfid', 'init', [arg0]);
 };
 
 exports.checkConnect = function (arg0, success, error) {
-  exec(success, error, "zebraRfid", "check_connect", [arg0]);
+    exec(success, error, 'zebraRfid', 'check_connect', [arg0]);
 };
 
 exports.connect = function (arg0, arg1, success, error) {
-  exec(success, error, "zebraRfid", "connect", [arg0, arg1]);
+    exec(success, error, 'zebraRfid', 'connect', [arg0, arg1]);
 };
 
 exports.disconnect = function (arg0, success, error) {
-  exec(success, error, "zebraRfid", "disconnect", [arg0]);
+    exec(success, error, 'zebraRfid', 'disconnect', [arg0]);
 };
 
 /*exports.change_mode = function (arg0, success, error) {
@@ -21,5 +21,5 @@ exports.disconnect = function (arg0, success, error) {
 };*/
 
 exports.change_power = function (arg0, success, error) {
-  exec(success, error, "zebraRfid", "change_power", [arg0]);
+    exec(success, error, 'zebraRfid', 'change_power', [arg0]);
 };
